f0c4ddd4423506df7c88dc690592365ceecb5113
Matt Graham <matthew.m.graham@gmail.com>
2019-12-11 10:17:02 +0800

    Adding setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='sde',
+    description='Utilities for inference in SDE models using Mici',
+    author='Matt Graham',
+    url='https://github.com/thiery-lab/manifold-mcmc-for-diffusions.git',
+    packages=['sde'],
+    python_requires='>=3.6',
+    install_requires=[
+        'mici=0.1.2', 'sympy>=1.4', 'numpy>=1.15', 'scipy>=1.1', 'jax=0.1.55'],
+    extras_require={
+        'notebook':  [
+            'matplotlib>=3.1', 'jupyter>=1.0', 'arviz>=0.5', 'corner>=2.0']
+    }
+
+)
