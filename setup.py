b388fe834820011953f2813f6c0058e64573d86a
Matt Graham <matthew.m.graham@gmail.com>
2021-10-01 00:25:52 +0100

    Update requirements and autodiscover package(s)

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='sde',
     description='Utilities for inference in SDE models using Mici',
     author='Matt Graham',
     url='https://github.com/thiery-lab/manifold-mcmc-for-diffusions.git',
-    packages=['sde'],
-    python_requires='>=3.6',
+    packages=find_packages(),
+    python_requires='>=3.7',
     install_requires=[
-        'mici>=0.1.8',  'numpy>=1.17', 'scipy>=1.1',
-        'jax>=0.1.55', 'jaxlib>=0.1.37'],
+        'mici==0.1.10',
+        'symnum==0.1.2',
+        'numpy==1.21',
+        'sympy==1.8',
+        'jax==0.2.21',
+        'jaxlib==0.1.71'
+    ],
     extras_require={
         'notebook':  [
-            'symnum>=0.1.1', 'sympy>=1.4', 'matplotlib>=3.1', 'jupyter>=1.0',
-            'arviz>=0.5', 'corner>=2.0']
+            'matplotlib==3.4',
+            'arviz==0.11.2',
+            'corner==2.2',
+            'jupyter>=1.0',
+        ],
+        'scripts': [
+            'matplotlib==3.4',
+            'arviz==0.11.2',
+            'pandas==1.3',
+        ]
     }
 )
