35fe7366ddaa9c85570cc9e020175908c4687696
Matt Graham <matthew.m.graham@gmail.com>
2021-09-30 23:00:09 +0100

    Remove unrequired import

--- scripts/fhn_model_noisy_obs_bridge_experiment.jl
+++ scripts/fhn_model_noisy_obs_bridge_experiment.jl
@@ -8,7 +8,6 @@ using BridgeSDEInference
 using BridgeSDEInference: indices
 using Random, LinearAlgebra, Distributions
 import Distributions.logpdf
-using Plots
 using NPZ
 using PyCall
 const State = SArray{Tuple{2},T,1,2} where {T}
@@ -157,7 +156,7 @@ dt_inference = T / (num_obs * args["num-steps-per-obs"])
 # Generate simulated data
 
 Random.seed!(args["seed-data"])
-X, _ = simulate_segment(0.0, x0_ref, P, tt); 
+X, _ = simulate_segment(0.0, x0_ref, P, tt);
 num_obs = 100
 num_steps_per_obs = div(length(tt), num_obs)
 obs_noise = [rand(Normal(0, 1)) for i in 1:num_obs]
@@ -165,7 +164,7 @@ obs_noise = [rand(Normal(0, 1)) for i in 1:num_obs]
 obs = (
     times = X.tt[num_steps_per_obs+1:num_steps_per_obs:end],
     values = [
-        ((L*x)[1] + σ_y * n) 
+        ((L*x)[1] + σ_y * n)
         for (x, n) in zip(X.yy[num_steps_per_obs+1:num_steps_per_obs:end], obs_noise)
     ]
 )
@@ -194,14 +193,14 @@ for c in 1:n_chain
     θ_init = θ_inits[c, :]
     P_trgt = FitzhughDiffusion(param, θ_init...)
     P_aux = [
-        FitzhughDiffusionAux(param, θ_init..., t₀, u, T, v) 
-        for (t₀, T, u, v) in zip(obs.times[1:end-1], obs.times[2:end], 
+        FitzhughDiffusionAux(param, θ_init..., t₀, u, T, v)
+        for (t₀, T, u, v) in zip(obs.times[1:end-1], obs.times[2:end],
                                  obs.values[1:end-1], obs.values[2:end])
     ]
     model_setup = DiffusionSetup(P_trgt, P_aux, PartObs())
     set_imputation_grid!(model_setup, dt_inference)
     set_observations!(
-        model_setup, [L for _ in P_aux], [Σ for _ in P_aux], 
+        model_setup, [L for _ in P_aux], [Σ for _ in P_aux],
         obs.values, obs.times)
     set_x0_prior!(model_setup, GsnStartingPt(mean(x0_prior), cov(x0_prior)), x0_init)
     initialise!(eltype(x0_init), model_setup, Vern7(), false, NoChangePt(100))
@@ -209,19 +208,19 @@ for c in 1:n_chain
     θ_updates = [
         ParamUpdate(
             MetropolisHastingsUpdt(), i, θ_init,
-            UniformRandomWalk(args["rwm-parameter-update-initial-step-size"], θ_is_nonnegative[name]), 
+            UniformRandomWalk(args["rwm-parameter-update-initial-step-size"], θ_is_nonnegative[name]),
             θ_priors[name],
             UpdtAuxiliary(Vern7(), check_if_recompute_ODEs(P_aux, i))
-        ) 
+        )
         for (i, name) in zip(θ_update_coords, θ_coord_names[θ_update_coords])
     ]
     mcmc_setup = MCMCSetup(
-        Imputation(NoBlocking(), args["pcn-imputation-update-initial-persistence"], 
+        Imputation(NoBlocking(), args["pcn-imputation-update-initial-persistence"],
         Vern7()), θ_updates...
     )
     schedule = MCMCSchedule(
         args["num-warm-up-iter"] + args["num-main-iter"], [[1, 2, 3, 4, 5]],
-        (save=args["num-iter-per-save-path"], verbose=args["num-iter-per-print-summary"], warm_up=0, 
+        (save=args["num-iter-per-save-path"], verbose=args["num-iter-per-print-summary"], warm_up=0,
         readjust=(x->((x%args["num-iter-per-readjust"]==0) && (x < args["num-warm-up-iter"]))), fuse=(x->false))
     )
     GC.gc()
@@ -234,9 +233,9 @@ num_path = size(mcmc_outputs[1][1].paths, 1)
 paths = permutedims(
     cat([
             cat([
-                    hcat([Array(x) for x in mcmc_outputs[c][1].paths[p]]...) 
+                    hcat([Array(x) for x in mcmc_outputs[c][1].paths[p]]...)
                     for p in 1:num_path
-                ]..., dims=3) 
+                ]..., dims=3)
             for c in 1:n_chain
         ]..., dims=4
     ), (4, 3, 2, 1)
@@ -246,7 +245,7 @@ traces = Dict(
     name=>hcat([[c[i] for c in output[2].θ_chain] for output in mcmc_outputs]...)'
     for (i, name) in zip(θ_update_coords, θ_coord_names[θ_update_coords]))
 npzwrite(
-    joinpath(output_dir, "traces.npz"), 
+    joinpath(output_dir, "traces.npz"),
     merge(traces, Dict("chain_times" => mcmc_times, "paths" => paths, "path_times" => path_times))
 )
 
